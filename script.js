1f83bfec8873a4913b3f334e1a8474223c2b8a66
Nick Lindau <nicholaithegreat@gmail.com>
2021-07-14 13:17:27 -0700

    add lesson 73(handle click functionality)

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct Number! 🎉'
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 13
-document.querySelector('.score').textContent = 10
-
-document.querySelector('.guess').value = 23
-console.log(document.querySelector('.guess').value);
-
 //?NOTE an event is something that happens on the page EX) mouse click, mouse moving, key press, button press. 
 //?NOTE an event listener is when we wait for an event to happen and then react to it
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = "🚫 No Number!"
+  }
+});
+
+//?NOTE because we are waiting for an event , the function won't run until the event happens
