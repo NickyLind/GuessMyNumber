5c4379b3f6982939ae90379fb6f5d4a37aa9751e
Nick Lindau <nicholaithegreat@gmail.com>
2021-07-14 13:04:21 -0700

    add lesson 72 functionality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number! 🎉'
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13
+document.querySelector('.score').textContent = 10
+
+document.querySelector('.guess').value = 23
+console.log(document.querySelector('.guess').value);
+
+//?NOTE an event is something that happens on the page EX) mouse click, mouse moving, key press, button press. 
+//?NOTE an event listener is when we wait for an event to happen and then react to it
